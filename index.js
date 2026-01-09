ed70269aade6d6f0d7796bf0196df3556f12edfc
Ahtisham Bashir <ahtishamdoggar@gmail.com>
2026-01-09 12:40:29 +0500

    strLengthIntro added

--- index.js
+++ index.js
@@ -9,5 +9,7 @@ const subject = "JavaScript";
 const topic = "strings";
 
 const sentence = `Today, you will learn about ${topic} in ${subject}.`;
-
 console.log(sentence);
+
+const strLengthIntro = `Here is an example of using the length property on the word ${subject}.`;
+console.log(strLengthIntro);
