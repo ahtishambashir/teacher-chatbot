1654b981b0a732876db8dd7db430d4a77103181c
Ahtisham Bashir <ahtishamdoggar@gmail.com>
2026-01-09 12:33:06 +0500

    Added the sentence variable

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const botName = "teacherBot";
 
 const greeting = `My name is ${botName}.`;
 console.log(greeting);
+
+const subject = "JavaScript";
+const topic = "strings";
+
+const sentence = `Today, you will learn about ${topic} in ${subject}.`;
+
+console.log(sentence);
