065d89e5898c75c6e1504e58feb98abf38f2493a
Ahtisham Bashir <ahtishamdoggar@gmail.com>
2026-01-09 18:36:28 +0500

    getting the index of the variables

--- index.js
+++ index.js
@@ -13,7 +13,15 @@ console.log(sentence);
 
 const strLengthIntro = `Here is an example of using the length property on the word ${subject}.`;
 console.log(strLengthIntro);
+
 console.log(subject.length);
 
 console.log(`Here is an example of using the length property on the word ${topic}.`);
 console.log(topic.length);
+
+console.log(`Here is an example of accessing the first letter in the word ${subject}.`);
+
+console.log(subject[0]);
+
+console.log(`Here is an example of accessing the second letter in the word ${subject}.`);
+console.log(subject[1]);
